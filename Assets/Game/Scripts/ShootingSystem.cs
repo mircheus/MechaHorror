d28d4d7ee37365d90c00fa926a1d8d6ebc24253d
Miroslav <mircheuss@yandex.ru>
2025-04-06 16:05:09 +0300

    Add basic LMB shoot trigger

---
+++ Assets/Game/Scripts/ShootingSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Scripts
+{
+    public class ShootingSystem : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference inputAction;
+        
+        private void OnEnable()
+        {
+            inputAction.action.Enable();
+            inputAction.action.performed += OnShoot;
+        }
+        
+        private void OnDisable()
+        {
+            inputAction.action.performed -= OnShoot;
+            inputAction.action.Disable();
+        }
+        
+        public void OnShoot(InputAction.CallbackContext context)
+        {
+            Debug.Log("Shooting action triggered!");
+        }
+    }
+}
