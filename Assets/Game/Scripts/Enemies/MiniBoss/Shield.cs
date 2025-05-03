4e4861fb7ce77fc504e5ea6bb838d4d8b3be132d
Miroslav <mircheuss@yandex.ru>
2025-05-03 17:03:05 +0300

    Add Shield color change on Damage Trigger

--- Assets/Game/Scripts/Enemies/MiniBoss/Shield.cs
+++ Assets/Game/Scripts/Enemies/MiniBoss/Shield.cs
@@ -7,10 +7,17 @@ namespace Game.Scripts.Enemies.MiniBoss.States
     [RequireComponent(typeof(BoxCollider))]
     public class Shield : MonoBehaviour
     {
+        [SerializeField] private Material[] damagedMaterials;
+        [SerializeField] private MeshRenderer meshRenderer;
+
+        private int _currentMatIndex = 0;
+
         public event Action ShieldHit;
 
         public void ActivateShield()
         {
+            _currentMatIndex = 0;
+            meshRenderer.material = damagedMaterials[_currentMatIndex];
             gameObject.SetActive(true);
         }
         
@@ -25,6 +32,17 @@ namespace Game.Scripts.Enemies.MiniBoss.States
             {
                 ShieldHit?.Invoke();
                 projectile.HitShield(this);
+                ChangeShieldColor();
+            }
+        }
+
+        private void ChangeShieldColor()
+        {
+            _currentMatIndex++;
+            
+            if (_currentMatIndex < damagedMaterials.Length)
+            {
+                meshRenderer.material = damagedMaterials[_currentMatIndex];
             }
         }
     }
