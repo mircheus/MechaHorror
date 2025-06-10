bfd82e0d30ec6892d74bd79ff2cf7707fdae916d
Miroslav <mircheuss@yandex.ru>
2025-06-10 22:09:25 +0300

    Add nearest and visible enemy lock-on

--- Assets/Game/Scripts/Player/AimLock.cs
+++ Assets/Game/Scripts/Player/AimLock.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Game.Scripts.Enemies._BaseEnemy;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,9 +10,11 @@ namespace Game.Scripts.Player
     public class AimLock : MonoBehaviour
     {
         [SerializeField] private float detectionRadius = 5f;
+        [SerializeField] private float dotThreshold = .5f;
         
+        private List<BaseEnemy> _detectedEnemies = new List<BaseEnemy>();
         private SphereCollider _sphereCollider;
-        
+
         public event UnityAction<Transform> TargetDetected;
         public event UnityAction TargetLost;
 
@@ -21,10 +24,19 @@ namespace Game.Scripts.Player
             _sphereCollider.radius = detectionRadius;
         }
 
+        private void Update()
+        {
+            if (_detectedEnemies.Count > 0)
+            {
+                DetectNearestEnemy();
+            }
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out BaseEnemy enemy))
             {
+                _detectedEnemies.Add(enemy);
                 TargetDetected?.Invoke(enemy.transform);
                 enemy.Death += OnEnemyDeath;
             }
@@ -36,12 +48,41 @@ namespace Game.Scripts.Player
             {
                 TargetLost?.Invoke();
                 enemy.Death -= OnEnemyDeath;
+                _detectedEnemies.Remove(enemy);
             }
         }
 
         private void OnEnemyDeath()
         {
             TargetLost?.Invoke();
+        }
+
+        private void DetectNearestEnemy()
+        {
+            float distance = -1;
+
+            foreach (var detectedEnemy in _detectedEnemies)
+            {
+                float currentDistance = Vector3.Distance(transform.position, detectedEnemy.transform.position);
+                
+                if (distance < 0 || currentDistance < distance)
+                {
+                    distance = currentDistance;
+                    
+                    if(IsEnemyVisible(detectedEnemy.transform))
+                    {
+                        TargetDetected?.Invoke(detectedEnemy.transform);
+                    }
+                }
+            }
+        }
+
+        private bool IsEnemyVisible(Transform enemy)
+        {
+            var transform1 = transform;
+            Vector3 directionToEnemy = enemy.position - transform1.position;
+            float dot = Vector3.Dot(transform1.forward, directionToEnemy.normalized);
+            return dot > dotThreshold; // Adjust this threshold as needed
         }
     }
 }
