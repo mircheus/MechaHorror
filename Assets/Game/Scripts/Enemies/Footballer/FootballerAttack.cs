133610bd0be28a620c4047f5f7c4ad4cb18e8050
Miroslav <mircheuss@yandex.ru>
2025-06-15 16:25:25 +0300

    Add car kick + rotation

--- Assets/Game/Scripts/Enemies/Footballer/FootballerAttack.cs
+++ Assets/Game/Scripts/Enemies/Footballer/FootballerAttack.cs
@@ -1,4 +1,6 @@
-﻿using Game.Scripts.Enemies._BaseEnemy;
+﻿using System.Collections;
+using Game.Scripts.Enemies._BaseEnemy;
+using Game.Scripts.Utilities;
 using UnityEngine;
 
 namespace Game.Scripts.Enemies.Footballer
@@ -11,6 +13,12 @@ namespace Game.Scripts.Enemies.Footballer
         [SerializeField] private Transform ballSpawnPoint;
         [SerializeField] private AnimationEventInvoker animationEventInvoker;
         
+        [Header("Kick Settings: ")]
+        [SerializeField] private float duration = 5f;
+        [SerializeField] private float height = 5f;
+        
+        private Coroutine _kickCoroutine;
+
         private void OnEnable()
         {
             animationEventInvoker.OnRangeAttack += OnAttack;
@@ -32,7 +40,48 @@ namespace Game.Scripts.Enemies.Footballer
         public override void Attack(Transform target)
         {
             var ball = Instantiate(ballPrefab, ballSpawnPoint.position, Quaternion.identity);
-            Debug.Log("Kick");
+
+            if (_kickCoroutine != null)
+            {
+                StopCoroutine(_kickCoroutine);
+            }
+            
+            _kickCoroutine = StartCoroutine(MoveInArc(ball.transform, ballSpawnPoint.position, target.position, duration, height));
         }
+        
+        private IEnumerator MoveInArc(Transform obj, Vector3 start, Vector3 end, float duration, float height)
+        {
+            float elapsed = 0f;
+
+            // XZ displacement
+            Vector3 startXZ = new Vector3(start.x, 0, start.z);
+            Vector3 endXZ = new Vector3(end.x, 0, end.z);
+            Vector3 deltaXZ = endXZ - startXZ;
+
+            float startY = start.y;
+            float endY = end.y;
+
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+                float t = Mathf.Clamp01(elapsed / duration);
+
+                // Base position along flat XZ plane
+                Vector3 currentXZ = startXZ + deltaXZ * t;
+
+                // Arc calculation
+                float arc = 4 * height * (t - t * t); // parabola with peak at t = 0.5
+
+                // Linear Y base + arc offset
+                float currentY = Mathf.Lerp(startY, endY, t) + arc;
+
+                obj.position = new Vector3(currentXZ.x, currentY, currentXZ.z);
+                yield return null;
+            }
+
+            obj.position = end;
+            obj.GetComponent<BallAutoRotate>().StopRotating();
+        }
+
     }
 }
---
+++ Assets/Game/Scripts/Utilities/BallAutoRotate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game.Scripts.Utilities
+{
+    public class BallAutoRotate : MonoBehaviour
+    {
+        [Header("Rotation Settings: ")]
+        [SerializeField] private Vector3 minRotationSpeed = new Vector3(180f, 180f, 0f); // Minimum rotation speed in degrees per second
+        [SerializeField] private Vector3 maxRotationSpeed = new Vector3(360f, 360f, 0f); // Maximum rotation speed in degrees per second
+
+        private Vector3 _rotationSpeed;
+        
+        private bool _isRotating = true;
+
+        private void OnEnable()
+        {
+            RandomizeRotationSpeed();
+        }
+
+        private void Update()
+        {
+            if (_isRotating)
+            {
+                transform.Rotate(_rotationSpeed * Time.deltaTime);
+            }
+        }
+
+        public void StopRotating()
+        {
+            _isRotating = false;
+        }
+        
+        private void RandomizeRotationSpeed()
+        {
+            _rotationSpeed = new Vector3(
+                Random.Range(minRotationSpeed.x, maxRotationSpeed.x), // Randomize X rotation speed
+                Random.Range(minRotationSpeed.y, maxRotationSpeed.y), // Randomize Y rotation speed
+                Random.Range(minRotationSpeed.z, maxRotationSpeed.z)  // Randomize Z rotation speed
+            );
+        }
+    }
+}
